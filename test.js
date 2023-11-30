b051beda7c0b53e97549dd2853fd4a32798226b2
Rehana Kabir Mim <rehanakabirmim@gmail.com>
2023-11-30 10:51:54 +0600

    namta print(1-10)

---
+++ test.js
@@ -0,0 +1,12 @@
+var x = 6;
+var y = 6;
+var z = x + y;
+console.log(`additon: ${z}`);
+
+const person = {
+    firstName: "John",
+    lastName: "Doe",
+    age: 50,
+    eyeColor: "blue"
+  };
+  console.log(typeof(person));
